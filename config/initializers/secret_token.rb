a6af3bde59bb1d17738d179a400c8cf765aa2b7b
GtK <GilliTheKid@GtK-MA.local>
2011-05-30 13:00:51 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cc2::Application.config.secret_token = 'f1bc3167dc0da10665a3b1967c653089744dc80bd0389858b91dfdd35c8c924591d05851fd93ff28345a68ffa991a0aac3e5aafd74bd481c41630d7db375525e'
