e9b1f8e618abbcaea592d0573992190cd6b4482b
GtK <GilliTheKid@GtK-MA.local>
2011-05-31 14:43:01 +0200

    Updated Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def profile
+  end
+
+  def account
+  end
+
+  def help
+  end
+
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'profile'" do
+    it "should be successful" do
+      get 'profile'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'account'" do
+    it "should be successful" do
+      get 'account'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
+end
